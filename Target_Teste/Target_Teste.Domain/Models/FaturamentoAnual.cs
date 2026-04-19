a806cb000975916711ad95272c9a98359146a313
Ricardo Junior <ricardo08jr@hotmail.com>
2026-04-19 16:37:11 -0300

    feat(domain): metodo para calcularResultado

--- Target_Teste/Target_Teste.Domain/Models/FaturamentoAnual.cs
+++ Target_Teste/Target_Teste.Domain/Models/FaturamentoAnual.cs
@@ -27,5 +27,38 @@ namespace Target_Teste.Domain.Models
 
             this.Valores = valores;
         }
+        public FaturamentoResult CalcularResultado()
+        {
+            decimal menor = decimal.MaxValue;
+            decimal maior = decimal.MinValue;
+            decimal soma = 0m;
+            int diasComFaturamento = 0;
+
+            foreach (var valor in Valores)
+            {
+                if (valor > 0)
+                {
+                    if (valor < menor) menor = valor;
+                    if (valor > maior) maior = valor;
+
+                    soma += valor;
+                    diasComFaturamento++;
+                }
+            }
+
+            if (diasComFaturamento == 0)
+                return new FaturamentoResult(0m, 0m, 0);
+
+            var media = soma / diasComFaturamento;
+            int diasAcimaDaMedia = 0;
+
+            foreach (var valor in Valores)
+            {
+                if (valor > 0 && valor > media)
+                    diasAcimaDaMedia++;
+            }
+
+            return new FaturamentoResult(menor, maior, diasAcimaDaMedia);
+        }
     }
 }
