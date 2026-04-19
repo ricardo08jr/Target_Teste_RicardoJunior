f1ed5353676292d752b0f1cd8302cbadfec6b3fb
Ricardo Junior <ricardo08jr@hotmail.com>
2026-04-19 16:42:16 -0300

    feat(application): add useCase calcularFaturamento

---
+++ Target_Teste/Target_Teste.Application/CalcularFaturamentoAnualUseCase.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Target_Teste.Application
+{
+    internal class CalcularFaturamentoAnualUseCase
+    {
+    }
+}
