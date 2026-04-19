d3ea056cb3f572096abc3226ea607b65cdb123f6
Ricardo Junior <ricardo08jr@hotmail.com>
2026-04-19 19:19:39 -0300

    feat: add docker compose

--- Target_Teste/Target_Teste.Api/obj/Debug/net8.0/Target_Teste.Api.AssemblyInfo.cs
+++ Target_Teste/Target_Teste.Api/obj/Debug/net8.0/Target_Teste.Api.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Target_Teste.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ee587464d5cfa9fe424b2dc74c3f889da6b16c5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9323b1873ce4be7eaef4d2781fc847da6ccef7d5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Target_Teste.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Target_Teste.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
