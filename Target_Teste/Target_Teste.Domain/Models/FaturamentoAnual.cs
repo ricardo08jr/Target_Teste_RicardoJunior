cd812eea73d952c90214c02dafc663853ef326db
Ricardo Junior <ricardo08jr@hotmail.com>
2026-04-19 16:34:47 -0300

    feat(domain): add atributos e construtor

---
+++ Target_Teste/Target_Teste.Domain/Models/FaturamentoAnual.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Target_Teste.Domain.DTOs;
+
+namespace Target_Teste.Domain.Models
+{
+    public class FaturamentoAnual
+    {
+        public decimal[] Valores { get; }
+
+        public FaturamentoAnual(decimal[] valores)
+        {
+            if (valores is null)
+            {
+                throw new ArgumentNullException(nameof(valores));
+            }
+               
+
+            if (valores.Length == 0)
+            {
+                throw new ArgumentException("O vetor de faturamento não pode ser vazio.", nameof(valores));
+            }
+               
+
+            this.Valores = valores;
+        }
+    }
+}
