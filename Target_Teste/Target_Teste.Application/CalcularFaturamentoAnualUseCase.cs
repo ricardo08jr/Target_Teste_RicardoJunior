72e1e5a33730c8e66e70715108a4ac83574db8d8
Ricardo Junior <ricardo08jr@hotmail.com>
2026-04-19 16:44:49 -0300

    feat(application): add logic do execute

--- Target_Teste/Target_Teste.Application/CalcularFaturamentoAnualUseCase.cs
+++ Target_Teste/Target_Teste.Application/CalcularFaturamentoAnualUseCase.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Target_Teste.Domain.DTOs;
+using Target_Teste.Domain.Models;
 
 namespace Target_Teste.Application
 {
-    internal class CalcularFaturamentoAnualUseCase
+    public class CalcularFaturamentoAnualUseCase
     {
+        public FaturamentoResultado Execute(decimal[] valores)
+        {
+            var faturamento = new FaturamentoAnual(valores);
+            return faturamento.CalcularResultado();
+        }
     }
 }
